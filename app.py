c44c98cbfed6ccc6e0e33b1fe400f880f7395fda
Sai Darshan <77203061+sai-darshan-k@users.noreply.github.com>
2024-09-07 11:04:45 +0530

    Updated app files and added my_model.keras to uploads

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def logout():
     flash('You have been logged out.', 'success')
     return redirect(url_for('login'))
 
-#if __name__ == '__main__':
- #   with app.app_context():
-  #      db.create_all()  # Create database tables
-    #app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables
+    app.run(debug=True, port=5000)
